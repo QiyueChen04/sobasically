3d27dd67c37b1d9bba18621850d51d227405b225
Qiyue Chen <jackchen20048@gmail.com>
2024-03-28 02:12:56 -0400

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 function Home() {
   return (
     <div>
       <Navbar></Navbar>
       <main><h1>Hello world</h1></main>
+      <Footer></Footer>
     </div>
   );
 }
