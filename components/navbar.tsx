5910732cb2e70d05addf479eebf03de7628089da
Qiyue Chen <jackchen20048@gmail.com>
2024-03-28 02:11:28 -0400

    maybe add logo to middle of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,11 +18,18 @@ function navbar() {
           </ul>
         </div>
       </div>
+      
       <div className="navbar-center">
+        {/* <div className="w-10">
+          <Link href={"../landingpage"}>
+            <img src={'/logo.png'} alt='SoBasically Logo'/>
+          </Link>
+        </div> */}
         <Link href={"../landingpage"}>
-          <p className="btn btn-ghost text-xl">SoBasically</p>
+            <p className="btn btn-ghost text-xl">SoBasically</p>
         </Link>
       </div>
+
       <div className="navbar-end">
         <button className="btn">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
