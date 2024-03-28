81add113387f7c354f98e0d94a52b6faff29a18b
Qiyue Chen <jackchen20048@gmail.com>
2024-03-28 00:45:56 -0400

    customized css

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -4,26 +4,32 @@ import { SparklesCore } from "../../components/ui/sparkles";
 
 function SparklesPreview() {
   return (
-    <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-        SoBasically
-      </h1>
-      <div className="w-[40rem] h-40 relative">
-        {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-        {/* Core component */}
+    <div className="h-screen w-full bg-black flex flex-col items-center justify-center overflow-hidden">
+      <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
+          id="tsparticlesfullpage"
           background="transparent"
-          minSize={0.4}
-          maxSize={1}
-          particleDensity={1200}
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
           className="w-full h-full"
           particleColor="#FFFFFF"
         />
+      </div>
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+        SoBasically
+      </h1>
+      
+      <div className="w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-4/5 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-4/5" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[8px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-5px w-1/4" />
+
+        <p className="p-7 font-light md:text-1xl sm:text-sm lg:text-2xl text-center text-white relative z-20">
+          Kickoff your Saturdays with a brief and unbiased economics article where we explore everything related to the dismal science
+        </p>
 
         {/* Radial Gradient to prevent sharp edges */}
         <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
