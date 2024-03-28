68b2024deaf7d5455adec743ee7720e0a7efac63
Qiyue Chen <jackchen20048@gmail.com>
2024-03-28 10:46:39 -0400

    edited home page formating

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,29 @@ import ThreeDCard from '../components/threedcard'
 
 function Home() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <div className='grid grid-cols-1 justfy-center'>
-        <div className="artboard artboard-horizontal phone-5 flex place-content-center self-center">
-          {/* <ThreeDCard></ThreeDCard>  */}
-        </div>
+    <div className='grid grid-cols-1 justify-center items-center'>
+      <div>
+        <Navbar></Navbar>
       </div>
+
+      <div className="grid place-items-center">
+        <ThreeDCard></ThreeDCard>
+        {/* <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div> */}
+      </div>
+
+      <div>
       <Footer></Footer>
+      </div>
+      
     </div>
   );
 }
