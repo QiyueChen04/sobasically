b66b87de11ac0aa165ae09e7c9fc072f08c5028e
Qiyue Chen <jackchen20048@gmail.com>
2024-03-28 18:37:15 -0400

    animated subscribe button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 
+import RoundSubButton from '../components/roundsubbutton'
+
 function navbar() {
   return (
     <div className="navbar bg-base-100">
@@ -31,10 +33,11 @@ function navbar() {
       </div>
 
       <div className="navbar-end">
-        <button className="btn btn-primary">
+        <RoundSubButton/>
+        {/* <button className="btn btn-primary">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
             Subscribe
-        </button> 
+        </button>  */}
         {/* <button className="btn btn-ghost btn-circle">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </button>
---
+++ components/roundsubbutton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function subbutton() {
+  return (
+    <div>
+      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+          Subscribe
+        </span>
+      </button> 
+    </div>
+  )
+}
+
+export default subbutton
