5442df8abb042071c24ce1307e6a5f85d2618ee9
Qiyue Chen <jackchen20048@gmail.com>
2024-03-29 02:01:21 -0400

    added header to qtm article

--- app/thequantitytheoryofmoney/page.tsx
+++ app/thequantitytheoryofmoney/page.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 import Navbar from '../../components/navbar'
 import Footer from '../../components/footer'
+import Header from '../../components/articles/header'
 
 function pages() {
   return (
-    <div>
+    <div className='grid grid-cols-1 align-items-center'>
       
       <Navbar/>
 
-      <div className='text-center'>
-        The Quantity Theory of Money
+      <div className='grid grid-cols-1 w-3/5 h-full place-self-center pt-7'>
+        <Header title='The Quantity Theory of Money' subtitle='Examining the money supply and inflation' date='Sep 02, 2023'/>
       </div>
 
       <Footer/>
