a95fe32008b7531adf2a582573ca29422d10e311
Qiyue Chen <jackchen20048@gmail.com>
2024-03-29 16:38:03 -0400

    added image component for articles

--- app/thequantitytheoryofmoney/page.tsx
+++ app/thequantitytheoryofmoney/page.tsx
@@ -6,6 +6,7 @@ import Footer from '../../components/footer'
 import Header from '../../components/articles/header'
 import Par from '../../components/articles/par'
 import Heading from '../../components/articles/heading'      
+import Img from '../../components/articles/image'
 
 function pages() {
   return (
@@ -16,14 +17,11 @@ function pages() {
       <div className='grid grid-cols-1 w-3/5 h-full place-self-center py-7'>
         <Header title='The Quantity Theory of Money' subtitle='Examining the money supply and inflation' date='Sep 02, 2023'/>
 
-
         <Par paragraph={
           <p>
             It’s widely accepted that the amount of money in circulation will impact the economy’s price level, but there is substantial disagreement over how strong this relationship is. If, for example, the amount of money doubles, will that double the economy’s price level? While the past several months of tightening monetary policy and a subsequent decrease in inflation may lead one to answer "yes," it’s important to consider the several additional factors that impact prices: demographic changes, geopolitics, aggregate supply, etc. This debate over the importance of the various causes of inflation leads us to one of the oldest theories in economics: the quantity theory of money.
           </p>
-        }
-        >
-        </Par>
+        }/>
 
         <Heading heading="The foundations"></Heading>
         
@@ -31,8 +29,15 @@ function pages() {
           <p>
             Dating back to the 16th-century writings of Nicolaus Copernicus (yes, the guy who came up with the sun-centric model of the universe), this deceptively simple concept emphasizes the relationship between inflation and the money supply. To be exact, it posits a direct relationship between the quantity of money in an economy and the general price level. However, the mathematical representation wasn’t formulated until the early 20th century by American economist Irving Fisher and his <strong>equation of exchange</strong>:
           </p>
-        }>
-        </Par>
+        }/>
+
+        <Img img='/qtm_mv=py.jpeg'></Img>
+
+        <Par paragraph={
+          <p>
+            M is the amount of money, V is the <strong>velocity of money </strong>(the rate at which money is used to purchase new goods and services), P is the price level, and Y is real GDP – hence the direct relationship between the money supply and prices. There are a few things to keep in mind here before going forward. First off, PY (the price level multiplied by the real GDP) is in fact the nominal GDP. This also means that the money supply multiplied by the velocity of money yields the nominal GDP – money velocity encompasses only activities that contribute to GDP, and so multiplying that number with how much money there is (specifically, money used to purchase new goods and services) equals the nominal GDP.
+          </p>
+        }/>
 
 
       </div>
---
+++ components/articles/image.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+
+function image({
+  img,
+}:{
+  img: string;
+}) {
+  return (
+    <div className='place-self-center py-5'>
+      <Image src={img} alt="article image" height="1000" width="300"></Image>
+    </div>
+  )
+}
+
+export default image
