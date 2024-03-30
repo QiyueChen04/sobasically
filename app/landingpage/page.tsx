05d7f9e9b38660d158d2e59381c3d215329479fe
Qiyue Chen <jackchen20048@gmail.com>
2024-03-30 15:27:37 -0400

    updated landingpage

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -26,9 +26,9 @@ function SparklesPreview() {
       <div className="w-[40rem] h-40 relative">
         {/* Gradients */}
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-4/5 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-4/5" />
+        <div className="inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-100" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[8px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-5px w-1/4" />
+        <div className="inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-5px w-100" />
 
         <p className="p-7 font-light md:text-1xl sm:text-sm lg:text-2xl text-center text-white relative z-20">
           Kickoff your Saturdays with a brief and unbiased economics article where we explore everything related to the dismal science
