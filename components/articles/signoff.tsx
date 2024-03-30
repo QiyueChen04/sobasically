a24404ecba251f0e32b7eaaa5606097ee93308d4
Qiyue Chen <jackchen20048@gmail.com>
2024-03-30 14:51:57 -0400

    added signoff comp

---
+++ components/articles/signoff.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Par from './par'
+
+function signoff() {
+  return (
+    <div>
+
+      <Par paragraph={
+        <p>
+          ‘Till next time, 
+        </p>
+      }/>
+
+      
+      <Par paragraph={
+        <p>
+          SoBasically
+        </p>
+      }/>
+    </div>
+  )
+}
+
+export default signoff
+
