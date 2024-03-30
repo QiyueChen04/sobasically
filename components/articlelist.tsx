f46a1b55f35e21a3a6d2bad3f89f3ed93676af95
Qiyue Chen <jackchen20048@gmail.com>
2024-03-30 16:05:13 -0400

    updated article list component

--- components/articlelist.tsx
+++ components/articlelist.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from 'react'
+import Link from 'next/link'
 
 interface Article { 
   title: string;
@@ -14,19 +15,43 @@ const list: Article[] = [
       title: 'Evergrande\'s Real Estate Crisis',
       subtitle: 'The perils of overleverage',
       image: '/Evergrande\'sRealEstateCrisis.jpeg',
-      date: 'AUG 26, 2023'
+      date: 'Aug 26, 2023'
     },
     {
-      title: 'Evergrande\'s Real Estate Crisis',
-      subtitle: 'The perils of overleverage',
-      image: '/Evergrande\'sRealEstateCrisis.jpeg',
-      date: 'AUG 26, 2023'
+      title: 'The Inflation Reduction Act',
+      subtitle: 'One year later',
+      image: '/thumbnail_theinflationreductionact.jpeg',
+      date: 'Aug 19, 2023'
     },
     {
-      title: 'Evergrande\'s Real Estate Crisis',
-      subtitle: 'The perils of overleverage',
-      image: '/Evergrande\'sRealEstateCrisis.jpeg',
-      date: 'AUG 26, 2023'
+      title: 'Option Strategies',
+      subtitle: 'Part 5: Butterfly Spreads',
+      image: '/thumbnail_opstrat.jpeg',
+      date: 'Aug 5, 2023'
+    },
+    {
+      title: 'Option Strategies',
+      subtitle: 'Part 4: Straddles and Strangles',
+      image: '/thumbnail_opstrat.jpeg',
+      date: 'Jul 29, 2023'
+    },
+    {
+      title: 'Option Strategies',
+      subtitle: 'Part 3: Bull and Bear Spreads',
+      image: '/thumbnail_opstrat.jpeg',
+      date: 'Jul 22, 2023'
+    },
+    {
+      title: 'Option Strategies',
+      subtitle: 'Part 2: Protective Options',
+      image: '/thumbnail_opstrat.jpeg',
+      date: 'Jul 15, 2023'
+    },
+    {
+      title: 'Option Strategies',
+      subtitle: 'Part 1: Covered Options',
+      image: '/thumbnail_opstrat.jpeg',
+      date: 'Jul 8, 2023'
     },
   ];
 
@@ -34,13 +59,16 @@ function articlelist() {
   return (
     <div className="pb-10">
       {list.map((article, index) => (
-        <div className="card card-side bg-base-100 shadow-xl py-5 items-center">
-          <figure><img src={article.image} alt="article image" className='rounded-xl'/></figure>
-          <div className="card-body">
-            <h2 className="card-title">{article.title}</h2>
-            <p>{article.subtitle}</p>
+        <Link href={'../comingsoon'}>
+          <div className="card card-side py-5 items-center rounded-xl hover:bg-primary hover:bg-opacity-50">
+            <img src={article.image} alt="article image" className='place-self-center rounded-xl w-60 h-50 object-cover'/>
+            <div className="card-body">
+              <h2 className="card-title text-white">{article.title}</h2>
+              <p>{article.subtitle}</p>
+              <p className='text-xs'>{article.date}</p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   )
