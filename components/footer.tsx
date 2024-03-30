0ce413a4ac488d83876401e0d94a5e72c5d81558
Qiyue Chen <jackchen20048@gmail.com>
2024-03-30 15:20:16 -0400

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -17,8 +17,8 @@ function footer() {
         </nav>  */}
         <nav>
           <h6 className="footer-title">SoBasically</h6> 
-          <Link href={"../comingsoon"}>Archieve</Link>
-          <Link href={"../comingsoon"}>Contact</Link>
+          <Link href={"../comingsoon"}>Archive</Link>
+          <Link href={'../aboutus'}>About</Link>
           {/* <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a> */}
         </nav> 
